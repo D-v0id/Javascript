27115663831ac49640e28c1fc5edc2e5043e0e73
Dv0id <leo.gomes08@gmail.com>
2024-12-09 01:20:30 -0300

    Primeira lição While

---
+++ While1.js
@@ -0,0 +1,16 @@
+//const nomeusuarios = ["Joana", "renata", "cleber", "Zeca"]
+
+//console.log('Olá', nomeusuarios[0])
+//console.log('Olá', nomeusuarios[1])
+//console.log('Olá', nomeusuarios[2])
+//console.log('Olá', nomeusuarios[3])
+const nomeusuarios = ["Joana", "renata", "cleber", "Zeca", "carla"]
+
+let IndiceUsuario = 0
+//let IndiceAtual = nomeusuarios.length
+
+while (IndiceUsuario < nomeusuarios.length) {
+    console.log('olá', nomeusuarios[IndiceUsuario])
+    //IndiceUsuario = IndiceUsuario + 1
+    IndiceUsuario += 1
+}
